b5f939cdda99ec3361815581494635e639627334
JuanFe622 <juanf.herrerac@autonoma.edu.co>
2022-08-18 08:28:45 -0500

    Create variables for capturing data from the registration form and make changes to index.html

---
+++ js/form.js
@@ -0,0 +1,16 @@
+let userName = document.getElementById("user-name").value;
+console.log(userName);
+let lastName = document.getElementById("last-name").value;
+console.log(lastName);
+let documentType = document.getElementById("document-type").value;
+console.log(documentType);
+let documentNumber = document.getElementById("document-number").value;
+console.log(documentNumber);
+let gender = document.getElementsByName("gender");
+
+for (var i = 0; i <  gender.length; i++) {
+    if (gender[i].checked) {
+      console.log(gender[i].value);
+      break;
+    }
+  }
