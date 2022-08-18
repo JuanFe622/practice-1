73851b387ee04970247f7b432b92e4b4c77f7724
JuanFe622 <juanf.herrerac@autonoma.edu.co>
2022-08-18 08:33:07 -0500

    Finalize data capture with form.js and index.html

--- js/form.js
+++ js/form.js
@@ -1,13 +1,17 @@
 let userName = document.getElementById("user-name").value;
 console.log(userName);
+
 let lastName = document.getElementById("last-name").value;
 console.log(lastName);
+
 let documentType = document.getElementById("document-type").value;
-console.log(documentType);
+let selectDocument = documentType.options[documentType.selectedIndex].text;
+console.log(selectDocument);
+
 let documentNumber = document.getElementById("document-number").value;
 console.log(documentNumber);
-let gender = document.getElementsByName("gender");
 
+let gender = document.getElementsByName("gender");
 for (var i = 0; i <  gender.length; i++) {
     if (gender[i].checked) {
       console.log(gender[i].value);
